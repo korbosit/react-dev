ba71bd07083269e350a89557daa7bbf8d8e4b172
Vlaimir Korba <krissky1986@gmail.com>
2023-09-23 16:20:33 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import { useState } from 'react';
+import { getImageUrl } from './utils.js';
 import './App.css';
 
-const person = {
-	name: 'Gregorio Y. Zara',
-	theme: {
-		 backgroundColor: 'black',
-		 color: 'pink',
-	},
-};
 
 
 function App() {
-	
    return (
-      <div style={person.theme}>
-         <h1>{person.name}'s Todos</h1>
-         <img
-            className="avatar"
-            src="https://i.imgur.com/7vQD0fPs.jpg"
-            alt="Gregorio Y. Zara"
+      <Card>
+         <Avatar
+				deb="true"
+            size={100}
+            person={{
+               name: "Katsuko Saruhashi",
+               imageId: "YfeOqp2",
+            }}
          />
-         <ul>
-            <li>Improve the videophone</li>
-            <li>Prepare aeronautics lectures</li>
-            <li>Work on the alcohol-fuelled engine</li>
-         </ul>
-      </div>
+      </Card>
    );
+}
+
+function Avatar({ person, size }) {
+	return (
+		<img
+			className="avatar"
+			src={getImageUrl(person)}
+			alt={person.name}
+			width={size}
+			height={size}
+		/>
+	);
+}
+
+function Card({ children }) {
+	return <div className="card">{children}</div>;
 }
 
 export default App;
