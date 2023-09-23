4807bca1c14531eb28463331830aef934288ccb8
Vlaimir Korba <krissky1986@gmail.com>
2023-09-23 15:42:00 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import './App.css';
 
 
@@ -17,7 +16,43 @@ import './App.css';
 //   );
 // }
 
-function MyButton() {
+//========================================================================================================================================================
+
+
+// function MyButton() {
+// 	const [count, setCount] = useState(0);
+
+// 	function handleClick() {
+// 		 setCount(count + 1);
+// 	}
+
+// 	return (
+// 		 <button onClick={handleClick}>
+// 			  Clicked {count} times
+// 		 </button>
+// 	);
+// }
+// function App() {
+//    return (
+//       <div>
+//          <h1>Counters that update separately</h1>
+//          <MyButton />
+//          <MyButton />
+//       </div>
+//    );
+// }
+
+//========================================================================================================================================================
+
+function MyButton({count, onClick }) {
+	return (
+		<button onClick={onClick}>
+			 Clicked {count} times
+		</button>
+  );
+}
+
+function App() {
 	const [count, setCount] = useState(0);
 
 	function handleClick() {
@@ -25,20 +60,12 @@ function MyButton() {
 	}
 
 	return (
-		 <button onClick={handleClick}>
-			  Clicked {count} times
-		 </button>
+		 <div>
+			  <h1>Counters that update together</h1>
+			  <MyButton count={count} onClick={handleClick} />
+			  <MyButton count={count} onClick={handleClick} />
+		 </div>
 	);
-}
-
-function App() {
-   return (
-      <div>
-         <h1>Counters that update separately</h1>
-         <MyButton />
-         <MyButton />
-      </div>
-   );
 }
 
 export default App;
