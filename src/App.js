ea613b26723bcb0b4e7781604e8e5132ccfd3f8a
Vlaimir Korba <krissky1986@gmail.com>
2023-09-23 15:25:41 +0300

    useState() - screen update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 
-function App() {
+// function App() {
+// 	const [count, setCount] = useState(0);
+
+// 	function handleClick () {
+// 		setCount ( count + 1 );
+// 	}
+
+// 	return (
+// 		<button onClick={handleClick}>
+// 			 Clicked {count} times
+// 		</button>
+//   );
+// }
+
+function MyButton() {
+	const [count, setCount] = useState(0);
+
 	function handleClick() {
-		alert('You clicked me!');
-  }
-   return <button onClick={handleClick}>Click me</button>;
+		 setCount(count + 1);
+	}
+
+	return (
+		 <button onClick={handleClick}>
+			  Clicked {count} times
+		 </button>
+	);
+}
+
+function App() {
+   return (
+      <div>
+         <h1>Counters that update separately</h1>
+         <MyButton />
+         <MyButton />
+      </div>
+   );
 }
 
 export default App;
