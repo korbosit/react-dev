8b9c314fab3c777535bbd414608db65b460a6b2c
Vlaimir Korba <krissky1986@gmail.com>
2023-09-23 15:15:02 +0300

    map, render <li>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+const products = [
+	{ title: 'Cabbage', isFruit: false, id: 1 },
+	{ title: 'Garlic', isFruit: false, id: 2 },
+	{ title: 'Apple', isFruit: true, id: 3 },
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const listItems = products.map((product) => (
+      <li
+         key={product.id}
+         style={{
+            color: product.isFruit ? "magenta" : "darkgreen",
+         }}
+      >
+         {product.title}
+      </li>
+   ));
+   return <ul>{listItems}</ul>;
 }
 
 export default App;
