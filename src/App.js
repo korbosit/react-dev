273e561dc7e3e98ee9bb168a02be905363a8ef8f
Vlaimir Korba <krissky1986@gmail.com>
2023-09-23 15:58:31 +0300

    JS in JSX in {}

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import { useState } from 'react';
 import './App.css';
 
-function Profile() {
-	return (
-		<img
-			src="https://i.imgur.com/MK3eW3As.jpg"
-			alt="Katherine Johnson"
-		/>
-	);
-}
+const person = {
+	name: 'Gregorio Y. Zara',
+	theme: {
+		 backgroundColor: 'black',
+		 color: 'pink',
+	},
+};
 
 
 function App() {
    return (
-      <section>
-         <h1>Amazing scientists</h1>
-         <Profile />
-         <Profile />
-         <Profile />
-      </section>
+      <div style={person.theme}>
+         <h1>{person.name}'s Todos</h1>
+         <img
+            className="avatar"
+            src="https://i.imgur.com/7vQD0fPs.jpg"
+            alt="Gregorio Y. Zara"
+         />
+         <ul>
+            <li>Improve the videophone</li>
+            <li>Prepare aeronautics lectures</li>
+            <li>Work on the alcohol-fuelled engine</li>
+         </ul>
+      </div>
    );
 }
 
